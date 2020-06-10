d3391ebf7e9a952cad2a21410f5f1c69487bae2b
Sergio Torso <sergiotorso@gmail.com>
2020-06-10 15:59:03 -0300

    Projeto criado

---
+++ src/main/java/com/r2d2/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.r2d2.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r2d2/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r2d2.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
